229c34c89d62b52a6462969fc3b6aa24ee5cb8ee
王少泽 <shaozewang@quarkfinance.com>
2017-11-21 13:08:18 +0800

    add mock test case

---
+++ template/test/unit/specs/Mock.spec.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+describe('Mock', () => {
+  it('mock should work', () => {
+    return Vue.prototype.$http.get('/users').then((res) => {
+      expect(res.data.data.length).to.equal(2){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+    }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+}){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
