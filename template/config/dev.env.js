304cabcaea89dd08c83f7624c497c22a12d491a8
王少泽 <shaozewang@quarkfinance.com>
2017-11-21 14:55:44 +0800

    修复env错误

--- template/config/dev.env.js
+++ template/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  URL_API:"http://localhost:8080/api"
+  URL_API: '"http://localhost:8080/api"'
 })
--- template/config/prod.env.js
+++ template/config/prod.env.js
@@ -1,5 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  URL_API:'"/"'
+  URL_API: '"/"'
 }
